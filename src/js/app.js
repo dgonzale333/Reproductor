61b7e184b60e1a232f814c1a37e889321432193e
David G <davidgonzalezdiaz2305@gmail.cm>
2024-10-23 10:26:00 -0500

    reto

---
+++ src/js/app.js
@@ -0,0 +1,51 @@
+import { playList } from "./playlist.js"
+
+const audio = document.getElementById('audio')
+const play = document.getElementById('play')
+const pause = document.getElementById('pause')
+const forward = document.getElementById('forward')
+const rewind = document.getElementById('rewind')
+const stop = document.getElementById('stop')
+const skip1 = document.getElementById('skip1')
+const skip2 = document.getElementById('skip2')
+
+play.addEventListener('click', () => audio.play())
+pause.addEventListener('click', () => audio.pause())
+rewind.addEventListener('click', () => audio.currentTime -= 10)
+forward.addEventListener('click', () => audio.currentTime += 10)
+stop.addEventListener('click', () => {
+    audio.pause()
+    audio.currentTime = 0
+})
+
+let currentSong = 0;
+
+function skipSong() {
+    const loadSong = playList[currentSong];
+
+    document.getElementById('imgSong').src = loadSong.img
+    document.getElementById('nameArtist').textContent = loadSong.title
+    document.getElementById('nameSong').textContent = loadSong.artist
+    audio.src = loadSong.song
+    audio.load()
+}
+
+skipSong();
+
+skip1.addEventListener('click', () => {
+    currentSong = (currentSong > 0) ? currentSong - 1 : playList.length - 1;
+    skipSong();
+});
+
+skip2.addEventListener('click', () => {
+    currentSong = (currentSong + 1) % playList.length;
+    skipSong();
+});
+
+// Para avanzar a la siguiente canción automáticamente
+audio.addEventListener('ended', () => {
+    currentSong = (currentSong + 1) % playList.length;
+    skipSong();
+});
+
+
